7a330e31d59dec8e570f12b7e5e796f528945fab
Jean-Frederic Clere <jfclere@gmail.com>
2018-08-16 18:21:30 +0200

    Add a reader and echo back what is read...

--- src/main/java/org/jfclere/asyncdemo/Servlet.java
+++ src/main/java/org/jfclere/asyncdemo/Servlet.java
@@ -9,7 +9,13 @@ import javax.servlet.AsyncEvent;
 import javax.servlet.AsyncListener;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
+
+import javax.servlet.ReadListener;
+
 import javax.servlet.annotation.WebServlet;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,6 +35,10 @@ public class Servlet extends HttpServlet implements AsyncListener {
             AsyncContext actx = req.startAsync();
             actx.addListener(this);
             resp.setContentType("text/plain");
+
+            DemoReader listener = new DemoReader(
+                actx, req.getInputStream(), resp.getOutputStream());
+
         } else {
             new Exception("Async Not Supported").printStackTrace();
             resp.sendError(400,"Async is not supported.");
@@ -53,5 +63,44 @@ public class Servlet extends HttpServlet implements AsyncListener {
     @Override
     public void onStartAsync(AsyncEvent event) throws IOException {
         System.out.println("onStartAsync");
+    }
+
+    private static class DemoReader implements ReadListener {
+        private final AsyncContext ac;
+        private final ServletInputStream sis;
+        private final ServletOutputStream sos;
+
+        private byte[] buffer = new byte[128];
+
+        private DemoReader(AsyncContext ac, ServletInputStream sis,
+                ServletOutputStream sos) {
+            this.ac = ac;
+            this.sis = sis;
+            this.sos = sos;
+            sis.setReadListener(this);
+        }
+
+        @Override
+        public void onDataAvailable() throws IOException {
+            System.out.println("onDataAvailable");
+            int read = 0;
+            while (sis.isReady() && read > -1) {
+                read = sis.read(buffer);
+                /* echo stuff back */
+                sos.write(buffer);
+                sos.flush();
+                System.out.println("onDataAvailable: read " + read);
+            }
+        }
+        @Override
+        public void onError(Throwable throwable) {
+            ac.complete();
+        }
+
+        @Override
+        public void onAllDataRead() throws IOException {
+            System.out.println("onAllDataRead");
+            ac.complete();
+        }
     }
 }
